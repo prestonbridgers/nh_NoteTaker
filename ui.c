e39f7e0b255cbb9add89662676a6fa7607b0ee8a
Curt Bridgers <prestonbridgers@gmail.com>
2024-01-31 19:33:28 -0500

    Tweaked the height ratio relative to the main window to make room for the abilities window

--- ui.c
+++ ui.c
@@ -49,9 +49,9 @@ nt_ui_create(NT_DATA *data)
     );
 
     // Resistances Window Initialization
-    ui->resistances_height = ui->main_height - 8;
+    ui->resistances_height = (ui->main_height - 8) / 2;
     ui->resistances_width = ui->main_width / 6;
-    ui->resistances_y = (ui->main_height - ui->resistances_height) / 2;
+    ui->resistances_y = (ui->main_height - ui->resistances_height) / 2 - 8;
     ui->resistances_x = 4;
     ui->W_resistances = derwin(
         ui->W_main,
